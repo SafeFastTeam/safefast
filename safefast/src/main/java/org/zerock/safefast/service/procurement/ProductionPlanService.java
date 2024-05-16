9131f2fd41fcd3ffbbfdfffebee20a62f8dcf9d7
MrHur <ekzmemforhs3@naver.com>
2024-05-16 10:21:44 +0900

    proucrement_ing

--- safefast/src/main/java/org/zerock/safefast/service/procurement/ProductionPlanService.java
+++ safefast/src/main/java/org/zerock/safefast/service/procurement/ProductionPlanService.java
@@ -2,8 +2,10 @@ package org.zerock.safefast.service.procurement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zerock.safefast.entity.ProcurementPlan;
 import org.zerock.safefast.entity.ProductionPlan;
 import org.zerock.safefast.repository.ProductionPlanRepository;
+import org.zerock.safefast.repository.ProcurementPlanRepository;
 
 import java.util.List;
 
@@ -11,6 +13,9 @@ import java.util.List;
 public class ProductionPlanService {
 
     private final ProductionPlanRepository productionPlanRepository;
+
+    @Autowired
+    private ProcurementPlanRepository procurementPlanRepository;
 
     @Autowired
     public ProductionPlanService(ProductionPlanRepository productionPlanRepository) {
@@ -23,5 +28,9 @@ public class ProductionPlanService {
 
     public List<ProductionPlan> getAllProductionPlans() {
         return productionPlanRepository.findAll();
+    }
+
+    public List<ProcurementPlan> getAllProcurementPlans() {
+        return procurementPlanRepository.findAll();
     }
 }
