a4396baa53182f5a8cbca2117fd7bfd1b2a07962
MrHur <ekzmemforhs3@naver.com>
2024-05-17 17:20:39 +0900

    dev purchase_order

--- src/main/java/org/zerock/safefast/controller/purchase_order/PurchaseOrderController.java
+++ src/main/java/org/zerock/safefast/controller/purchase_order/PurchaseOrderController.java
@@ -4,8 +4,11 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zerock.safefast.entity.ProcurementPlan;
+import org.zerock.safefast.entity.PurchaseOrder;
 import org.zerock.safefast.service.procurement.ProcurementPlanService;
 
 import java.util.List;
@@ -17,6 +20,12 @@ public class PurchaseOrderController {
 
     @Autowired
     private ProcurementPlanService procurementPlanService;
+
+    @PostMapping("/purchase_order")
+    public String createPurchaseOrder(@ModelAttribute PurchaseOrder purchaseOrder, Model model) {
+        model.addAttribute("purchaseOrder", purchaseOrder);
+        return "redirect:/purchase_order/purchase_order";
+    }
 
     @GetMapping("/purchase_order")
     public String showPurchaseOrderPage(Model model) {
