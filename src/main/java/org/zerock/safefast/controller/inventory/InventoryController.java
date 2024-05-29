451278833163d211cb1593f570a0c0c5886605b2
haejun lee <jihae185@gmail.com>
2024-05-29 17:53:43 +0900

    ing

--- src/main/java/org/zerock/safefast/controller/inventory/InventoryController.java
+++ src/main/java/org/zerock/safefast/controller/inventory/InventoryController.java
@@ -1,35 +1,48 @@
 package org.zerock.safefast.controller.inventory;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.safefast.entity.InventoryItem;
+import org.zerock.safefast.entity.Releases;
 import org.zerock.safefast.service.inventory.InventoryService;
+import org.zerock.safefast.service.releases.ReleasesService;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
+@Log4j2
 public class InventoryController {
 
     private final InventoryService inventoryService;
-
-    @Autowired
-    public InventoryController(InventoryService inventoryService) {
-        this.inventoryService = inventoryService;
-    }
+    private final ReleasesService releasesService;
 
     // 재고 페이지를 보여주는 메서드
     @GetMapping("/inventory")
     public String showInventoryPage(Model model) {
         // InventoryService를 통해 재고 데이터를 가져옵니다.
-        List<InventoryItem> inventoryItems = inventoryService.getAllInventoryItems();
+        List<Releases> releasesList = inventoryService.getAllReleases();
+
+        Map<String, Integer> itemCodeQuantities  = new HashMap<>();
+        for (Releases release : releasesList) {
+            String itemCode = release.getItem().getItemCode();
+            Integer quantity = release.getReleaseQuantity();
+            itemCodeQuantities.put(itemCode, itemCodeQuantities.getOrDefault(itemCode, 0) + quantity);
+        }
 
         // 모델에 재고 데이터를 추가하여 뷰에 전달합니다.
-        model.addAttribute("inventoryItems", inventoryItems);
+        model.addAttribute("releases", releasesList);
+        model.addAttribute("itemCodeQuantities", itemCodeQuantities);
 
         return "/inventory/inventory"; // inventory.html 템플릿을 반환합니다.
     }
