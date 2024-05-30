fb5e058b37c231eadcba32f9f3deffb3718355be
MrHur <ekzmemforhs3@naver.com>
2024-05-31 04:20:37 +0900

    uploading files and loading images

--- src/main/java/org/zerock/safefast/controller/contract/ContractController.java
+++ src/main/java/org/zerock/safefast/controller/contract/ContractController.java
@@ -1,31 +1,42 @@
 package org.zerock.safefast.controller.contract;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.safefast.service.contract.ContractService;
 import org.zerock.safefast.entity.CoOpCompany;
 import org.zerock.safefast.entity.Contract;
 import org.zerock.safefast.entity.Item;
 import org.zerock.safefast.repository.CoOpCompanyRepository;
-import org.zerock.safefast.repository.ContractRepository;
 import org.zerock.safefast.repository.ItemRepository;
-import org.zerock.safefast.service.contract.ContractService;
-import org.zerock.safefast.service.product.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
-import java.time.LocalDate;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/contract")
 @RequiredArgsConstructor
 @Log4j2
 public class ContractController {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     private final ContractService contractService;
     private final CoOpCompanyRepository coOpCompanyRepository;
@@ -43,8 +54,7 @@ public class ContractController {
     @GetMapping("/company")
     @ResponseBody
     public ResponseEntity<CoOpCompany> getCompanyByBusinessNumber(@RequestParam("businessNumber") String businessNumber) {
-        CoOpCompany company = coOpCompanyRepository.findById(businessNumber)
-                .orElse(null);
+        CoOpCompany company = coOpCompanyRepository.findById(businessNumber).orElse(null);
         if (company != null) {
             return ResponseEntity.ok(company);
         } else {
@@ -66,7 +76,6 @@ public class ContractController {
         Item item = itemRepository.findById(itemCode)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid Item Code: " + itemCode));
 
-
         Contract contract = Contract.builder()
                 .coOpCompany(coOpCompany)
                 .item(item)
@@ -80,5 +89,46 @@ public class ContractController {
         contractService.registerContract(contract, contractFile);
 
         return "redirect:/contract/register";
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("파일이 비어 있습니다.");
+        }
+
+        try {
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            return ResponseEntity.ok(fileName);
+        } catch (IOException e) {
+            log.error("파일을 저장하는 동안 오류가 발생했습니다.", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 저장 중 오류 발생");
+        }
+    }
+
+    @GetMapping("/file/{fileName}")
+    @ResponseBody
+    public ResponseEntity<Resource> getImage(@PathVariable String fileName) throws IOException {
+        Resource resource = contractService.loadFileAsResource(fileName);
+        String encodedFileName = encodeFileName(resource.getFilename());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename*=UTF-8''" + encodedFileName)
+                .body(resource);
+    }
+    private String encodeFileName(String fileName) {
+        try {
+            return URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString()).replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Failed to encode file name", e);
+        }
     }
 }
--- src/main/java/org/zerock/safefast/service/contract/ContractService.java
+++ src/main/java/org/zerock/safefast/service/contract/ContractService.java
@@ -3,22 +3,24 @@ package org.zerock.safefast.service.contract;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.safefast.entity.Contract;
 import org.zerock.safefast.entity.Item;
 import org.zerock.safefast.repository.ContractRepository;
-import org.springframework.web.multipart.MultipartFile;
 import org.zerock.safefast.repository.ItemRepository;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +29,9 @@ public class ContractService {
     private static final Logger log = LoggerFactory.getLogger(ContractService.class);
     private final ContractRepository contractRepository;
     private final ItemRepository itemRepository;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     public List<Contract> getAllContracts() {
         return contractRepository.findAll();
@@ -37,15 +42,12 @@ public class ContractService {
     }
 
     public void registerContract(Contract contract, MultipartFile file) {
-
         contract.setContractNumber(generateNextContractNumber());
         contract.setContractDate(LocalDate.now());
-
         contractRepository.save(contract);
 
         if (!file.isEmpty()) {
             try {
-                String uploadDir = "uploads";
                 String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
                 Path uploadPath = Paths.get(uploadDir);
 
@@ -60,8 +62,14 @@ public class ContractService {
                 contractRepository.save(contract);
             } catch (IOException e) {
                 log.error("파일을 저장하는 동안 오류가 발생했습니다.", e);
+                throw new RuntimeException("Failed to store file", e);
             }
         }
+    }
+
+    public byte[] getContractFile(String filename) throws IOException {
+        Path filePath = Paths.get(uploadDir, filename);
+        return Files.readAllBytes(filePath);
     }
 
     private String generateNextContractNumber() {
@@ -71,6 +79,23 @@ public class ContractService {
             return String.format("CON-%03d", nextNumber);
         } else {
             return "CON-001";
+        }
+    }
+
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = Paths.get(uploadDir).resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new IOException("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException("File not found " + fileName, ex);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
